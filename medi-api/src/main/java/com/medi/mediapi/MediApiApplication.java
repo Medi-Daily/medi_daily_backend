c68932d86b3cfe2f91d48cb08614959457fbba0b
Donghun Won <wonhun1225@gmail.com>
2024-12-15 19:27:06 +0900

    Init : 초기 세팅

---
+++ medi-api/src/main/java/com/medi/mediapi/MediApiApplication.java
@@ -0,0 +1,13 @@
+package com.medi.mediapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediApiApplication.class, args);
+    }
+
+}
